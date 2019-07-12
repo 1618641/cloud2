889f7b6249fa6a1226006a0c691f69b6204f2a1b
1618641 <1618641@qq.com>
2019-07-13 00:30:51 +0800

    111111111

---
+++ src/main/java/org/java/j59/cloud_eureka/CloudEurekaApplication.java
@@ -0,0 +1,13 @@
+package org.java.j59.cloud_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaApplication.class, args);
+    }
+
+}
